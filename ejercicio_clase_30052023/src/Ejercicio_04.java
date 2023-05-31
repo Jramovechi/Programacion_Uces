af5dd8a751a611632c7daf90b9e57376c5ec2e78
Jonathan Ramovechi <89465298+Jramovechi@users.noreply.github.com>
2023-05-30 22:11:52 -0300

    Actualizacion Ej_04

--- ejercicio_clase_30052023/src/Ejercicio_04.java
+++ ejercicio_clase_30052023/src/Ejercicio_04.java
@@ -2,14 +2,13 @@ public class Ejercicio_04 {
     public static void main(String[] args) {
         String [] array1 = {"mario", "marcos", "roberto", "juan"};
 
-
         int contador = 0;
 
-        for (int i = 0; i < array1.length; i++) {
-            String palabra = array1[i];
+        for (String palabra : array1){
 
             for (int j = 0; j < palabra.length(); j++){
-                if ((palabra.charAt(j) == 'a') || (palabra.charAt(j) =='e') || (palabra.charAt(j) =='i') || (palabra.charAt(j) =='o') || (palabra.charAt(j) =='u')){
+                char letra = Character.toLowerCase(palabra.charAt(j)) ;
+                if ((letra == 'a') || (letra =='e') || (letra =='i') || (letra =='o') || (letra =='u')){
                     contador++;
                 }
                 System.out.println("El array contiene " + contador + " vocales");
